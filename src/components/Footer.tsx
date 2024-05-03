da8a657309499a204a05fd6d41ce98c8a88214ff
george-valente <valentegeorge71@gmail.com>
2024-05-03 14:41:22 -0400

    Finalizado

---
+++ src/components/Footer.tsx
@@ -0,0 +1,58 @@
+import { Dribbble, Facebook, Github, Instagram, Twitter } from "lucide-react";
+
+export function Footer(){
+    return(
+        <div className="max-w-[1240px]  mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+            <div>
+                <h1 className = "w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+                <p className="py-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quo ex possimus! Fugit atque unde perferendis tempora pariatur fuga hic?</p>
+                <div className = "flex justify-between md:w-[75%] my-6">
+                    <Facebook className="cursor-pointer" />
+                    <Instagram className="cursor-pointer" />
+                    <Twitter className="cursor-pointer" />
+                    <Github className="cursor-pointer" />
+                    <Dribbble className="cursor-pointer" />
+                </div>
+            </div>
+
+            <div className="lg:col-span-2  flex justify-between mt-6">
+                <div>
+                    <h6 className = "font-medium text-gray-400">Solutions</h6>
+                    <ul>
+                        <li className="py-2 text-sm">Analytics</li>
+                        <li className="py-2 text-sm">Marketing</li>
+                        <li className="py-2 text-sm">Commerce</li>
+                        <li className="py-2 text-sm">Insights </li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className = "font-medium text-gray-400">Suport</h6>
+                    <ul>
+                        <li className="py-2 text-sm">Pricing</li>
+                        <li className="py-2 text-sm">Documentation</li>
+                        <li className="py-2 text-sm">Guides</li>
+                        <li className="py-2 text-sm">API Status </li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className = "font-medium text-gray-400">Company </h6>
+                    <ul>
+                        <li className="py-2 text-sm">About</li>
+                        <li className="py-2 text-sm">Blog</li>
+                        <li className="py-2 text-sm">Jobs</li>
+                        <li className="py-2 text-sm">Press </li>
+                        <li className="py-2 text-sm">Careers </li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className = "font-medium text-gray-400">Legal</h6>
+                    <ul>
+                        <li className="py-2 text-sm">Claim</li>
+                        <li className="py-2 text-sm">Policy</li>
+                        <li className="py-2 text-sm">Terms</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
